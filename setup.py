85442e9bc49243043b942926be5c834d04cf4295
trytodupe <liuandi@live.com>
2025-07-06 22:01:57 +0800

    chore: dependencies in setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,9 @@ setup(
     name="amis-python",
     version="1.0.9",
     packages=find_packages(),
+    install_requires=[
+        "pydantic>=1.9.2,<2.0.0",
+        "jinja2>=3.1.2,<4.0.0",
+        "python>=3.7,<4.0"
+    ],
 )
