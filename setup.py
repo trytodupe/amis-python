c9048668cede0c1d0c4d4694178404e7cfa2937a
trytodupe <liuandi@live.com>
2025-07-06 23:57:32 +0800

    chore: bump dependency version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="1.0.9",
     packages=find_packages(),
     install_requires=[
-        "pydantic>=1.9.2,<2.0.0",
+        "pydantic>=2.0.0,<3.0.0",
         "jinja2>=3.1.2,<4.0.0"
     ],
 )
