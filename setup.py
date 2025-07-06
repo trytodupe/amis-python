cd0b7aa5f43e29e4b64c06d8b4d4e41fe4bebeaf
trytodupe <liuandi@live.com>
2025-07-07 00:26:19 +0800

    fix: include template folder

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(
     name="amis-python",
     version="1.0.9",
     packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "amis": ["templates/*.jinja2"],
+    },
     install_requires=[
         "pydantic>=2.0.0,<3.0.0",
         "jinja2>=3.1.2,<4.0.0"
