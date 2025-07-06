d6880ab81b949239c83a6b19cd6666dfe1f5a71d
trytodupe <liuandi@live.com>
2025-07-06 22:04:17 +0800

    chore: remove python from dependencies

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
     packages=find_packages(),
     install_requires=[
         "pydantic>=1.9.2,<2.0.0",
-        "jinja2>=3.1.2,<4.0.0",
-        "python>=3.7,<4.0"
+        "jinja2>=3.1.2,<4.0.0"
     ],
 )
